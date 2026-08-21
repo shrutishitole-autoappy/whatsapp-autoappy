5d571cfb1fee63a9f025749a1e5e0a191ba9d0d8
shrutishitole20 <shrutishitole@example.com>
2026-08-21 22:54:18 +0530

    AUTO-COS-000:Indexjs code

---
+++ src/features/business/index.js
@@ -0,0 +1,15 @@
+import BusinessAutomation from "./components/BusinessAutomation";
+
+export { default as BusinessAutomation } from "./components/BusinessAutomation";
+export { default as BusinessHeader } from "./components/BusinessHeader";
+export { default as BusinessSidebar } from "./components/BusinessSidebar";
+export { default as WorkflowPage } from "./components/WorkflowPage";
+export { default as CustomAutomationPage } from "./components/CustomAutomationPage";
+export { default as StepCard } from "./components/StepCard";
+export { default as BottomStats } from "./components/BottomStats";
+export { default as AutoCursor } from "./components/AutoCursor";
+export { Pill, IconCircle, Block } from "./components/UIPrimitives";
+export { useAutoPilot, sleep } from "./hooks/useAutoPilot";
+export * from "./constants/businessData";
+
+export default BusinessAutomation;
